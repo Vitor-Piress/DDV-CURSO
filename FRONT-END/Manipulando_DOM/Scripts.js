9a1a889b71915294081f4e2c290200b2bede17ff
Vitor Pires <vitorspires25@gmail.com>
2026-04-05 17:59:30 -0300

    Update conditionals in To Do List

--- FRONT-END/Manipulando_DOM/Scripts.js
+++ FRONT-END/Manipulando_DOM/Scripts.js
@@ -52,24 +52,27 @@ function TaskCardConstructor(title) {
   return html;
 }
 
-function Alert(text) {
-  const html = `<div id="alert">
-      <svg
-        class="alert-icon"
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-      >
-        <circle cx="12" cy="12" r="10"></circle>
-        <path d="m15 9-6 6"></path>
-        <path d="m9 9 6 6"></path>
-      </svg>
+function Alert(text, color) {
+  blockAlert = `<path d="m15 9-6 6"></path>
+  <path d="m9 9 6 6"></path>`;
+  infoAlert = `<path d="M12 16v-4"></path>
+  <path d="M12 8h.01"></path>`;
+  let reason;
+
+  switch (color) {
+    case "red":
+      reason = blockAlert;
+      break;
+    case "blue":
+      reason = infoAlert;
+      break;
+  }
+
+  const html = `<div id="alert" class="${color}">
+      <svg class="alert-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+  <circle cx="12" cy="12" r="10"></circle>
+  ${reason}
+</svg>
       ${text}
     </div>`;
 
@@ -81,7 +84,7 @@ submit_button.addEventListener("click", () => {
   if (task_input.value == "") {
     body.insertAdjacentHTML(
       "beforeend",
-      Alert("Você não pode inserir uma tarefa vazia!"),
+      Alert("Você não pode inserir uma tarefa vazia!", "red"),
       setTimeout(() => {
         const alert = document.getElementById("alert");
         alert.style.opacity = "0";
@@ -90,6 +93,19 @@ submit_button.addEventListener("click", () => {
       }, 3000),
     );
     return;
+  }
+
+  if (task_input.value.length >= 34) {
+    body.insertAdjacentHTML(
+      "beforeend",
+      Alert("Cuidado ao adicionar títulos muito extensos!", "blue"),
+      setTimeout(() => {
+        const alert = document.getElementById("alert");
+        alert.style.opacity = "0";
+
+        setTimeout(() => alert.remove(), 1000);
+      }, 3000),
+    );
   }
   task_section.insertAdjacentHTML(
     "afterbegin",
@@ -121,13 +137,16 @@ task_section.addEventListener("click", (event) => {
     if (task_input.value == "") {
       body.insertAdjacentHTML(
         "beforeend",
-        Alert("Você não pode alterar uma tarefa para uma tarefa vazia!"),
+        Alert(
+          "Para alterar uma tarefa, digite o novo título no campo de entrada!",
+          "blue",
+        ),
         setTimeout(() => {
           const alert = document.getElementById("alert");
           alert.style.opacity = "0";
 
           setTimeout(() => alert.remove(), 1000);
-        }, 3000),
+        }, 5000),
       );
       return;
     }
